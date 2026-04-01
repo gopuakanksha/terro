42a713189651a71aa88a8372a56893a11afcb781
gopuakanksha <gopuakanksha379@gmail.com>
2026-04-01 20:10:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
-provider="aws"{
-region ="us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
+ 
+resource "aws_instance" "example" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+}
+ 
+
